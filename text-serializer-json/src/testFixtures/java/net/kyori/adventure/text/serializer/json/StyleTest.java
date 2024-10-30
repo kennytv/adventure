4d0cb7498e5b45f90921c35b4e5c3e7d604d6140
Kieran Wallbanks <kieran.wallbanks@gmail.com>
2024-10-30 18:16:21 +0000

    remove unused import

--- text-serializer-json/src/testFixtures/java/net/kyori/adventure/text/serializer/json/StyleTest.java
+++ text-serializer-json/src/testFixtures/java/net/kyori/adventure/text/serializer/json/StyleTest.java
@@ -35,7 +35,6 @@ import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
