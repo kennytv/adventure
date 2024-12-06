8856286e0ee83c64c881b8432f2be06e6da6452e
zml <zml@stellardrift.ca>
2024-12-05 19:38:16 -0800

    shadow color: style issues

--- api/src/main/java/net/kyori/adventure/text/format/ShadowColor.java
+++ api/src/main/java/net/kyori/adventure/text/format/ShadowColor.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of adventure, licensed under the MIT License.
  *
- * Copyright (c) 2017-2023 KyoriPowered
+ * Copyright (c) 2017-2024 KyoriPowered
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,15 +25,40 @@ package net.kyori.adventure.text.format;
 
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+/**
+ * A shadow color which may be applied to a {@link Style}.
+ *
+ * <p>Similar to {@link TextColor}, except that shadows contain alpha information.</p>
+ *
+ * @since 4.18.0
+ * @sinceMinecraft 1.21.4
+ */
 public interface ShadowColor extends StyleBuilderApplicable {
+  /**
+   * Create a new shadow color from the ARGB value packed in an int.
+   *
+   * @param value the int-packed ARGB value
+   * @return a new shadow colour
+   * @since 4.18.0
+   */
   @Contract(pure = true)
-  static ShadowColor shadowColor(final int value) {
+  static @NotNull ShadowColor shadowColor(final int value) {
     return new ShadowColorImpl(value);
   }
 
+  /**
+   * Attempt to parse a shadow colour from a {@code #}-prefixed hex string.
+   *
+   * <p>This string should be in the format {@code #RRGGBBAA}</p>
+   *
+   * @param value the input value
+   * @return a shadow color if possible, or null if any components are invalid
+   * @since 4.18.0
+   */
   @Contract(pure = true)
-  static ShadowColor fromHexString(String value) {
+  static @Nullable ShadowColor fromHexString(@NotNull String value) {
     if (value.startsWith("#")) {
       value = value.substring(1);
     }
@@ -41,26 +66,39 @@ public interface ShadowColor extends StyleBuilderApplicable {
       return null;
     }
     try {
-      int r = Integer.parseInt(value.substring(0, 2), 16);
-      int g = Integer.parseInt(value.substring(2, 4), 16);
-      int b = Integer.parseInt(value.substring(4, 6), 16);
-      int a = Integer.parseInt(value.substring(6, 8), 16);
+      final int r = Integer.parseInt(value.substring(0, 2), 16);
+      final int g = Integer.parseInt(value.substring(2, 4), 16);
+      final int b = Integer.parseInt(value.substring(4, 6), 16);
+      final int a = Integer.parseInt(value.substring(6, 8), 16);
       return new ShadowColorImpl((a << 24) | (r << 16) | (g << 8) | b);
     } catch (NumberFormatException ignored) {
       return null;
     }
   }
 
-  @NotNull
-  default String asHexString() {
-    int argb = value();
-    int a = (argb >> 24) & 0xFF;
-    int r = (argb >> 16) & 0xFF;
-    int g = (argb >> 8) & 0xFF;
-    int b = argb & 0xFF;
+  /**
+   * Represent this shadow color as a {@code #}-prefixed hex string.
+   *
+   * <p>This string will be in the format {@code #RRGGBBAA}</p>
+   *
+   * @return the hex string representation of this shadow colour
+   * @since 4.18.0
+   */
+  default @NotNull String asHexString() {
+    final int argb = this.value();
+    final int a = (argb >> 24) & 0xFF;
+    final int r = (argb >> 16) & 0xFF;
+    final int g = (argb >> 8) & 0xFF;
+    final int b = argb & 0xFF;
     return String.format("#%02X%02X%02X%02X", r, g, b, a);
   }
 
+  /**
+   * The int-packed ARGB value of this shadow colour.
+   *
+   * @return the shadow colour value
+   * @since 4.18.0
+   */
   int value();
 
   @Override
